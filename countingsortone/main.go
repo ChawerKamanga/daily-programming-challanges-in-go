2ef4f7bd283ce01ee06e5eb6a3d04346b4c843a5
Chawer Kamanga <chawerkamanga@gmail.com>
2024-02-01 17:29:42 +0200

    updated readme.md

---
+++ countingsortone/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'countingSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func countingSort(arr []int32) []int32 {
+	// Initialize a new result array with 100 elements, all set to 0
+	result := make([]int32, 100)
+
+	// Loop through each element in the input array
+	for _, value := range arr {
+		// Increment the count of the element's value in the result array
+		result[value] += 1
+	}
+
+	// Return the result array which now contains the frequency of each number
+	return result
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var arr []int32
+
+	for i := 0; i < int(n); i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	result := countingSort(arr)
+
+	for i, resultItem := range result {
+		fmt.Fprintf(writer, "%d", resultItem)
+
+		if i != len(result)-1 {
+			fmt.Fprintf(writer, " ")
+		}
+	}
+
+	fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ countingsortone/readme.md
@@ -0,0 +1,11 @@
+# Counting Sort 1
+- Given a list of integers, count and return the number of times each value appears as an array of integers.
+
+![Screenshot of the Challange from Hackerank](screenshot.png)
+
+## My Thinking
+1. Initialize a new result array with 100 elements, all set to 0.
+2. Loop through the input array (arr).
+3. For each element in arr, use its value as the index in the result array and increment the value at that index by 1.
+4. Return the result array which now contains the frequency of each number in the input array.
+
